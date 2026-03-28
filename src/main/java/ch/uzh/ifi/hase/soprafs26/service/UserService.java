7c31135f97ebc76664febc87e7ea564076fe725e
Zara <zara.ahmed@uzh.ch>
2026-03-28 20:17:43 +0100

    registration and error handling #15, #19

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/UserService.java
@@ -1,5 +1,9 @@
 package ch.uzh.ifi.hase.soprafs26.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,9 +16,6 @@ import ch.uzh.ifi.hase.soprafs26.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
 
-import java.util.List;
-import java.util.UUID;
-
 /**
  * User Service
  * This class is the "worker" and responsible for all functionality related to
@@ -23,32 +24,34 @@ import java.util.UUID;
  * to the caller.
  */
 @Service
-@Transactional
+@Transactional //method treated as whole if gone wrong - rollback
 public class UserService {
 
-	private final Logger log = LoggerFactory.getLogger(UserService.class);
+	private final Logger log = LoggerFactory.getLogger(UserService.class); // creates logger for log messages 
 
-	private final UserRepository userRepository;
+	private final UserRepository userRepository; //variable for userrep
 
 	public UserService(@Qualifier("userRepository") UserRepository userRepository) {
-		this.userRepository = userRepository;
+		this.userRepository = userRepository; //injects userrep so can b used 
 	}
 
 	public List<User> getUsers() {
-		return this.userRepository.findAll();
+		return this.userRepository.findAll(); //call findall from userrep
 	}
 
 	public User createUser(User newUser) {
+		checkIfUserExists(newUser); //cheks if exist
 		newUser.setToken(UUID.randomUUID().toString());
-		newUser.setStatus(UserStatus.OFFLINE);
-		checkIfUserExists(newUser);
+		newUser.setStatus(UserStatus.ONLINE);
+		newUser.setCreationDate(LocalDate.now()); //these things not provided by client
+		
 		// saves the given entity but data is only persisted in the database once
 		// flush() is called
-		newUser = userRepository.save(newUser);
-		userRepository.flush();
+		newUser = userRepository.save(newUser); //prep saving to database
+		userRepository.flush(); //executes statment
 
 		log.debug("Created Information for User: {}", newUser);
-		return newUser;
+		return newUser; // r
 	}
 
 	/**
@@ -59,20 +62,16 @@ public class UserService {
 	 *
 	 * @param userToBeCreated
 	 * @throws org.springframework.web.server.ResponseStatusException
-	 * @see User
+	 * @see User //refrence link
 	 */
 	private void checkIfUserExists(User userToBeCreated) {
 		User userByUsername = userRepository.findByUsername(userToBeCreated.getUsername());
-		User userByName = userRepository.findByName(userToBeCreated.getName());
+		
 
 		String baseErrorMessage = "The %s provided %s not unique. Therefore, the user could not be created!";
-		if (userByUsername != null && userByName != null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-					String.format(baseErrorMessage, "username and the name", "are"));
-		} else if (userByUsername != null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format(baseErrorMessage, "username", "is"));
-		} else if (userByName != null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format(baseErrorMessage, "name", "is"));
-		}
+		if (userByUsername != null) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT,
+					String.format(baseErrorMessage, "username", "is"));
 	}
+}
 }
