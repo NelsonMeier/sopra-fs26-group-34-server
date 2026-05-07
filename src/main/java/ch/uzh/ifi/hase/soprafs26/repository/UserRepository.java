31d9fb6332d8be151c319be25a7c399e14b869f4
nelsonmeier <nelsonlinus.meier@uzh.ch>
2026-05-07 19:36:09 +0200

    Finalized backend fot friendsOnly Scoreboard and Added custom queries(Issue https://github.com/NelsonMeier/sopra-fs26-group-34-client/issues/98)

--- src/main/java/ch/uzh/ifi/hase/soprafs26/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/repository/UserRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ch.uzh.ifi.hase.soprafs26.entity.User;
@@ -19,8 +20,18 @@ public interface UserRepository extends JpaRepository<User, Long> {
     
     @Query("SELECT u FROM User u WHERE u.typingHighScore IS NOT NULL ORDER BY u.typingHighScore DESC")
     List<User> findTopTypingSpeedScores(Pageable pageable);
+
 	List<User> findByUsernameStartingWithIgnoreCase(String prefix);
 
 	@Query("SELECT u FROM User u WHERE u.timeIntervalHighScore IS NOT NULL ORDER BY u.timeIntervalHighScore ASC")
 	List<User> findTopTimeIntervalScores(Pageable pageable);
+
+	@Query("SELECT u FROM User u JOIN Friend f ON f.friend = u WHERE f.user.id = :userId AND u.reactionHighScore IS NOT NULL ORDER BY u.reactionHighScore ASC")
+	List<User> findFriendsTopReactionTimeScores(@Param("userId") Long userId, Pageable pageable);
+
+	@Query("SELECT u FROM User u JOIN Friend f ON f.friend = u WHERE f.user.id = :userId AND u.typingHighScore IS NOT NULL ORDER BY u.typingHighScore DESC")
+	List<User> findFriendsTopTypingSpeedScores(@Param("userId") Long userId, Pageable pageable);
+
+	@Query("SELECT u FROM User u JOIN Friend f ON f.friend = u WHERE f.user.id = :userId AND u.timeIntervalHighScore IS NOT NULL ORDER BY u.timeIntervalHighScore ASC")
+	List<User> findFriendsTopTimeIntervalScores(@Param("userId") Long userId, Pageable pageable);
 }
